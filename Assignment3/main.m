f4600dc23ccf26ba0e87f393d6897b0af9916739
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 23:20:17 -0400

    Macros.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -5,95 +5,15 @@
 /* This my block comment.
  It can span multiple lines. */
 
+#define PI 3.14159
+#define RAD_TO_DEG(radians) (radians * (180.0 / PI))
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        double odometer = 9200.8;
-        int odometerAsInteger = (int)odometer;
-        
-        NSLog(@"You've driven %.1f miles", odometer);        // 9200.8
-        NSLog(@"You've driven %d miles", odometerAsInteger); // 9200
-        
-        double const pi = 3.14159;
-        // pi = 42001.0;               // Compiler error
-        
-        NSLog(@"6 + 2 = %d",  6 + 2);    // 8
-        NSLog(@"6 - 2 = %d",  6 - 2);    // 4
-        NSLog(@"6 * 2 = %d",  6 * 2);    // 12
-        NSLog(@"6 / 2 = %d",  6 / 2);    // 3
-        NSLog(@"6 %% 2 = %d", 6 % 2);    // 0
-        
-        int i = 0;
-        NSLog(@"%d", i);    // 0
-        i++;
-        NSLog(@"%d", i);    // 1
-        i++;
-        NSLog(@"%d", i);    // 2
-        
-        int modelYear = 1990;
-        if (modelYear < 1967) {
-            NSLog(@"That car is an antique!!!");
-        } else if (modelYear <= 1991) {
-            NSLog(@"That car is a classic!");
-        } else if (modelYear == 2013) {
-            NSLog(@"That's a brand new car!");
-        } else {
-            NSLog(@"There's nothing special about that car.");
-        }
-        
-        /* a == b  Equal to
-        a != b  Not equal to
-        a > b   Greater than
-        a >= b  Greater than or equal to
-        a < b   Less than
-        a <= b  Less than or equal to
-        !a  Logical negation
-        a && b  Logical and
-        a || b  Logical or */
-        
-        // Switch statements (only work with integral types)
-        switch (modelYear) {
-            case 1987:
-                NSLog(@"Your car is from 1987.");
-                break;
-            case 1988:
-                NSLog(@"Your car is from 1988.");
-                break;
-            case 1989:
-            case 1990:
-                NSLog(@"Your car is from 1989 or 1990.");
-                break;
-            default:
-                NSLog(@"I have no idea when your car was made.");
-                break;
-        }
-        
-        // While loops
-        // int i = 0;       already defined in line 26.
-        while (i<5) {
-            if (i == 3) {
-                NSLog(@"Aborting the while-loop");
-                break;
-            }
-            NSLog(@"Current year: %d", modelYear + i);
-            i++;
-        }
-        
-        // For loops
-        
-        for (int i=0; i<5; i++) {
-            if (i == 3) {
-                NSLog(@"Skipping a for-loop iteration");
-                continue;
-            }
-            NSLog(@"Current year: %d", modelYear + i);
-        }
-        
-        NSArray *models = @[@"Ford", @"Honda", @"Nissan", @"Porsche"];
-        for (id model in models) {
-            NSLog(@"%@", model);
-        }
-        
+        double angle = PI / 2;              // 1.570795
+        NSLog(@"%f", RAD_TO_DEG(angle));    // 90.0
     }
     return 0;
 }
