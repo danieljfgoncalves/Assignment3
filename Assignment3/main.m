d684239394b1cff2f5e1f9ac1f1ccb557bd020e7
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 22:51:33 -0400

    My Comments, '// Inline' & '/* Block */'.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -1,12 +1,9 @@
-//
-//  main.m
-//  Assignment3
-//
-//  Created by Daniel Goncalves on 2015-04-07.
-//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+
+// This is my inline comment
+
+/* This my block comment.
+ It can span multiple lines. */
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
