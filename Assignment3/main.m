1e3970ad88959f24f55bac61b7977e8e53d33ffe
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 23:09:01 -0400

    Operators & Switch Statements

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -41,6 +41,33 @@ int main(int argc, const char * argv[]) {
             NSLog(@"There's nothing special about that car.");
         }
         
+        /* a == b  Equal to
+        a != b  Not equal to
+        a > b   Greater than
+        a >= b  Greater than or equal to
+        a < b   Less than
+        a <= b  Less than or equal to
+        !a  Logical negation
+        a && b  Logical and
+        a || b  Logical or */
+        
+        // Switch statements (only work with integral types)
+        switch (modelYear) {
+            case 1987:
+                NSLog(@"Your car is from 1987.");
+                break;
+            case 1988:
+                NSLog(@"Your car is from 1988.");
+                break;
+            case 1989:
+            case 1990:
+                NSLog(@"Your car is from 1989 or 1990.");
+                break;
+            default:
+                NSLog(@"I have no idea when your car was made.");
+                break;
+        }
+        
         
     }
     return 0;
