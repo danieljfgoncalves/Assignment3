0f3cfec27b92105fdd9861db9aeb085200e5f0ee
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-08 00:01:16 -0400

    Void Pointers.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -4,9 +4,9 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int year = 1967;
-        int *pointer = &year;
-        NSLog(@"%d", *pointer);     // Do something with the value
-        pointer = NULL;             // Then invalidate it
+        void *genericPointer = &year;
+        int *intPointer = (int *)genericPointer;
+        NSLog(@"%d", *intPointer);
     }
     return 0;
 }
