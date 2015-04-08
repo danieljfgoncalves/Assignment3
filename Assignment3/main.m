b1e817710624d09ba66c8237cbd58269c3500e20
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 23:05:33 -0400

    Conditionals

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -15,7 +15,7 @@ int main(int argc, const char * argv[]) {
         NSLog(@"You've driven %d miles", odometerAsInteger); // 9200
         
         double const pi = 3.14159;
-        pi = 42001.0;               // Compiler error
+        // pi = 42001.0;               // Compiler error
         
         NSLog(@"6 + 2 = %d",  6 + 2);    // 8
         NSLog(@"6 - 2 = %d",  6 - 2);    // 4
@@ -29,6 +29,19 @@ int main(int argc, const char * argv[]) {
         NSLog(@"%d", i);    // 1
         i++;
         NSLog(@"%d", i);    // 2
+        
+        int modelYear = 1990;
+        if (modelYear < 1967) {
+            NSLog(@"That car is an antique!!!");
+        } else if (modelYear <= 1991) {
+            NSLog(@"That car is a classic!");
+        } else if (modelYear == 2013) {
+            NSLog(@"That's a brand new car!");
+        } else {
+            NSLog(@"There's nothing special about that car.");
+        }
+        
+        
     }
     return 0;
 }
