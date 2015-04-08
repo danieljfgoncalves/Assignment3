0c2ca15753f9782228bc6c69c13f229631e31465
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 22:56:00 -0400

    Odometer variable.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -8,7 +8,11 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        double odometer = 9200.8;
+        int odometerAsInteger = (int)odometer;
+        
+        NSLog(@"You've driven %.1f miles", odometer);        // 9200.8
+        NSLog(@"You've driven %d miles", odometerAsInteger); // 9200
     }
     return 0;
 }
