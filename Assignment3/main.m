50ae69078926b949d9096546de04ebd0b6000d33
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 23:54:06 -0400

    Pointers 2.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -3,12 +3,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int year = 1967;          // Define a normal variable
-        int *pointer;             // Declare a pointer that points to an int
-        pointer = &year;          // Find the memory address of the variable
-        NSLog(@"%d", *pointer);   // Dereference the address to get its value
-        *pointer = 1990;          // Assign a new value to the memory address
-        NSLog(@"%d", year);       // Access the value via the variable
+        char model[5] = {'H', 'o', 'n', 'd', 'a'};
+        char *modelPointer = &model[0];
+        for (int i=0; i<5; i++) {
+            NSLog(@"Value at memory address %p is %c",
+                  modelPointer, *modelPointer);
+            modelPointer++;
+        }
+        NSLog(@"The first letter is %c", *(modelPointer - 5));
     }
     return 0;
 }
