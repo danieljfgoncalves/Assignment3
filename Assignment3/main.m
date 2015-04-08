9ffe01573003ab2b49f191efd37d4af97356265e
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 23:22:44 -0400

    Typedef.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -1,20 +1,14 @@
 #import <Foundation/Foundation.h>
 
-// This is my inline comment
-
-/* This my block comment.
- It can span multiple lines. */
-
-#define PI 3.14159
-#define RAD_TO_DEG(radians) (radians * (180.0 / PI))
-
+typedef unsigned char ColorComponent;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        double angle = PI / 2;              // 1.570795
-        NSLog(@"%f", RAD_TO_DEG(angle));    // 90.0
+        ColorComponent red = 255;
+        ColorComponent green = 160;
+        ColorComponent blue = 0;
+        NSLog(@"Your paint job is (R: %hhu, G: %hhu, B: %hhu)",
+              red, green, blue);
     }
     return 0;
 }
-
