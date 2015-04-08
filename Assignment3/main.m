7791055c7d15e1b9cf48da9d038de8c34687ca29
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-08 00:04:13 -0400

    Pointers in Obj-C & NULL for C & nil for Obj-C.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -3,10 +3,15 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int year = 1967;
-        void *genericPointer = &year;
-        int *intPointer = (int *)genericPointer;
-        NSLog(@"%d", *intPointer);
+        NSString *model = @"Honda";
+        
+        NSString *anObject;    // An Objective-C object
+        anObject = NULL;       // This will work
+        anObject = nil;        // But this is preferred
+        int *aPointer;         // A plain old C pointer
+        aPointer = nil;        // Don't do this
+        aPointer = NULL;       // Do this instead
+        
     }
     return 0;
 }
