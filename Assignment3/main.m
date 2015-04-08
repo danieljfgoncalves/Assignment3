9888bce8c656adb6c6b1643f406990ed3135f50e
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 23:35:19 -0400

    Structs.

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -1,14 +1,16 @@
 #import <Foundation/Foundation.h>
 
-typedef unsigned char ColorComponent;
+typedef struct {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+} Color;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        ColorComponent red = 255;
-        ColorComponent green = 160;
-        ColorComponent blue = 0;
+        Color carColor = {255, 160, 0};
         NSLog(@"Your paint job is (R: %hhu, G: %hhu, B: %hhu)",
-              red, green, blue);
+              carColor.red, carColor.green, carColor.blue);
     }
     return 0;
 }
